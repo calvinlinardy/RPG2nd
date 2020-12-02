a3c72c896223e3be215886e92161aedb4fcaaabf
calvinlinardy <calvin.linardy@yahoo.com>
2020-12-02 21:01:54 +0800

    Player Movement
    - added movement animation (left, right, up, down).
    - added movement script.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float movementSpeed = 5f;
+    float movementX = 0;
+    float movementY = 0;
+
+    //Cache references
+    Rigidbody2D myRb;
+    Animator myAnim;
+
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
+
+        myRb.velocity = new Vector2(movementX, movementY) * movementSpeed;
+
+        myAnim.SetFloat("moveX", myRb.velocity.x);
+        myAnim.SetFloat("moveY", myRb.velocity.y);
+
+        if (movementX == 1 || movementX == -1 || movementY == 1 || movementY == -1)
+        {
+            myAnim.SetFloat("lastMoveX", movementX);
+            myAnim.SetFloat("lastMoveY", movementY);
+        }
     }
 }
