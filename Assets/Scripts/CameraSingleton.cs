89637413bc9f4ba1f9857a8a001367a26e9d2bd0
calvinlinardy <calvin.linardy@yahoo.com>
2021-02-23 23:04:36 +0800

    More Updates
    - teleport bugs fixed.
    - added fade transition while loading scene.
    - added collider at all of the edges of the map.

---
+++ Assets/Scripts/CameraSingleton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraSingleton : MonoBehaviour
+{
+    private void Awake()
+    {
+        int cameraCount = FindObjectsOfType<Camera>().Length;
+        if (cameraCount > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
