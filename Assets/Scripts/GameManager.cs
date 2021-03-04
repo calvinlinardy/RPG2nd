7649e5b41e27e9e4e350ac8791a91f279661d3fc
calvinlinardy <calvin.linardy@yahoo.com>
2021-03-04 23:11:37 +0800

    menu 2.1
    - building inventory system.
    - items panel is not empty anymore.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : MonoBehaviour
     public CharStats[] playerStats;
 
     public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
+
+    public string[] itemHeld;
+    public int[] numberOfItem;
     // Start is called before the first frame update
     void Start()
     {
