1a51dc2a7970faf8741a06e9904128eb7e107e29
calvinlinardy <calvin.linardy@yahoo.com>
2021-05-22 07:52:08 +0800

    FINAL (Thesis)
    - coins regenerate time length in dungeon nerfed. (takes longer)
    - coins value in dungeon nerfed, (less value)
    - dialog starter bug fixed.
    - main menu bug fixed. (accident)

--- Assets/Scripts/ForestDialogs.cs
+++ Assets/Scripts/ForestDialogs.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ForestDialogs : MonoBehaviour
 {
+    public GameObject dialogs;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,11 @@ public class ForestDialogs : MonoBehaviour
         Scene scene = SceneManager.GetActiveScene();
         if (scene.name != "Forest")
         {
-            gameObject.SetActive(false);
+            dialogs.gameObject.SetActive(false);
+        }
+        else
+        {
+            dialogs.gameObject.SetActive(true);
         }
     }
 }
